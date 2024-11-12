d927d08e476dcfc3f803a59ea827a2cc837b4205
dchuquilla <dario.chuquilla@gmail.com>
2024-11-12 17:18:36 -0500

    fix user migration and schema; update password_digest column to allow null values and correct schema definition

--- backend/db/migrate/20241112144256_create_users.rb
+++ backend/db/migrate/20241112144256_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name, null: false
       t.string :email, null: false, unique: true
-      t.string :password_digest, null: false
+      t.string :password_digest, null: true
       t.string :role, null: false, default: "member", limit: 20, index: true
 
       t.timestamps
