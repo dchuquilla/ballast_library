8554a7d7e35c167bafbe6235ee1ed7d5ab47bd01
dchuquilla <dario.chuquilla@gmail.com>
2024-11-12 21:16:12 -0500

    add association between Book and BookCopy models; implement validation for presence of book in BookCopy

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_many :book_copies, dependent: :destroy
+
   validates :title, :author, :genre, :isbn, presence: true
   validates :isbn, uniqueness: true
   validates :total_copies, numericality: { greater_than_or_equal_to: 0 }
--- backend/spec/models/book_copy_spec.rb
+++ backend/spec/models/book_copy_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe BookCopy, type: :model do
     book_copy = FactoryBot.create(:book_copy, book: book)
     expect(book_copy).to be_valid
   end
+
+  it "is not valid without a book" do
+    book_copy = FactoryBot.build(:book_copy, book: nil)
+    expect(book_copy).not_to be_valid
+  end
 end
