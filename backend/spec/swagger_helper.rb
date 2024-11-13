3e36cb23e687b2465c4e6e53349fa0c04f1a8a9a
dchuquilla <dario.chuquilla@gmail.com>
2024-11-13 04:29:08 -0500

    feat: update Swagger configuration to use JSON format for API documentation

--- backend/spec/swagger_helper.rb
+++ backend/spec/swagger_helper.rb
@@ -18,34 +18,34 @@ RSpec.configure do |config|
   # the root example_group in your specs, e.g. describe '...', openapi_spec: 'v2/swagger.json'
 
   config.openapi_specs = {
-    "v1/swagger.yaml" => {
-      openapi: "3.0.1",
-      info: {
-        title: "API V1",
-        version: "v1",
-      },
-      paths: {},
-      servers: [
-        {
-          url: "http://{defaultHost}",
-          variables: {
-            defaultHost: {
-              default: "127.0.0.1:3000",
-            },
-          },
-        },
-      ],
-      components: {
-        securitySchemes: {
-          Bearer: {
-            description: "...",
-            type: :apiKey,
-            name: "authorization",
-            in: :header,
-          },
-        },
-      },
-    },
+    # "v1/swagger.yaml" => {
+    #   openapi: "3.0.1",
+    #   info: {
+    #     title: "API V1",
+    #     version: "v1",
+    #   },
+    #   paths: {},
+    #   servers: [
+    #     {
+    #       url: "http://{defaultHost}",
+    #       variables: {
+    #         defaultHost: {
+    #           default: "127.0.0.1:3000",
+    #         },
+    #       },
+    #     },
+    #   ],
+    #   components: {
+    #     securitySchemes: {
+    #       Bearer: {
+    #         description: "...",
+    #         type: :apiKey,
+    #         name: "authorization",
+    #         in: :header,
+    #       },
+    #     },
+    #   },
+    # },
     "v1/swagger.json" => {
       openapi: "3.0.1",
       info: {
